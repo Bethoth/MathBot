ec65dc766bb7d93e6231b459ebb33c67adc24a79
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-03-10 20:25:30 +0100

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function squarePerimeter(side) {
+    return side * 4;
+}
+
+function rectanglePerimeter(length, width) {
+    return (length * 2) + (width * 2);
+}
