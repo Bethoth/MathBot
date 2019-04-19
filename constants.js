15770270c3d6b8c0486b8608bcda37096c64d4b3
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-04-19 15:51:46 +0200

    Change the prefixes

--- constants.js
+++ constants.js
@@ -1,7 +1,9 @@
-const prefix = "math";
+const lPrefix = "math";
+const uPrefix = "Math";
 const pi = Math.PI;
 
 module.exports = {
-    prefix : prefix,
+    lPrefix : lPrefix,
+    uPrefix : uPrefix,
     pi : pi
 }
