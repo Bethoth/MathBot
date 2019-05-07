2af95188d1b1d1fb60cf0c4f3ced532a61baf432
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-05-07 21:52:33 +0200

    Add the function `map` for the percentages

--- calcFunctions.js
+++ calcFunctions.js
@@ -18,10 +18,15 @@ function modulo(a, b) {
     return Math.round((a % b) * 1000) / 1000;
 }
 
+function map(value, start1, stop1, start2, stop2) {
+    return 1.0 * start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+}
+
 module.exports = {
     add : add,
     subtract : subtract,
     multiply : multiply,
     divide : divide,
-    modulo : modulo
+    modulo : modulo,
+    percentageAugmentation : map
 }
