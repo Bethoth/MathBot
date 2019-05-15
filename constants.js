ebd739d148fd203da95a67f0415fc7b60a44e39d
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-05-15 07:41:19 +0200

    Add the square root sign : "\u221a"

--- constants.js
+++ constants.js
@@ -2,10 +2,12 @@ const lPrefix = "math";
 const uPrefix = "Math";
 const pi = Math.PI;
 const lPi = "\u03c0";
+const squareRoot = "\u221a"
 
 module.exports = {
     lPrefix : lPrefix,
     uPrefix : uPrefix,
     pi : pi,
-    litteralPi : lPi
+    litteralPi : lPi,
+    squareRoot : squareRoot
 }
