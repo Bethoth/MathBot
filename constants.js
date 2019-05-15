58ec8c767de07faaaed035f57db15be00a6eeb93
Bethoth <45798269+ThePizzaDu62@users.noreply.github.com>
2019-05-15 07:42:24 +0200

    Add the times sign : "\u00d7"

--- constants.js
+++ constants.js
@@ -2,12 +2,14 @@ const lPrefix = "math";
 const uPrefix = "Math";
 const pi = Math.PI;
 const lPi = "\u03c0";
-const squareRoot = "\u221a"
+const squareRoot = "\u221a";
+const times = "\u00d7";
 
 module.exports = {
     lPrefix : lPrefix,
     uPrefix : uPrefix,
     pi : pi,
     litteralPi : lPi,
-    squareRoot : squareRoot
+    squareRoot : squareRoot,
+    times : times
 }
